aebdb5533998c09d9a516f65f6e53e980282f4a2
Abdulbosit <abdulbosit19980204@gmail.com>
2023-03-08 22:48:04 +0500

    message

---
+++ js.js
@@ -0,0 +1,26 @@
+let numberOfSeries = +prompt("nechta serial kordingiz", ""),
+
+    seriesDB = {
+        count: numberOfSeries,
+        series: {},
+        actors: {},
+        genres: [],
+        privat: false,
+    }
+
+// a = prompt("Oxirgi ko'rgan serialingiz?", ""),
+// c = prompt("Nechi baxo berasiz?", ""),
+// b = prompt("Oxirgi ko'rgan serialingiz?", ""),
+// d = prompt("Nechi baxo berasiz?", "")
+
+// seriesDB.series[a] = c
+// seriesDB.series[b] = d
+
+for (i = 0; i < numberOfSeries; i++) {
+    let a = prompt(i + 1 + " - Ko'rgan serialingiz?", ""),
+        c = prompt(i + 1 + " - serialga Nechi baxo berasiz?", "")
+    seriesDB.series[a] = c
+        // seriesDB.series[b] = d
+}
+
+console.log(numberOfSeries, seriesDB);
