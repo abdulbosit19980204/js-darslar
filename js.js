b33c77ddb2210f704f313d6a536680c38b1bd7f1
Abdulbosit <abdulbosit19980204@gmail.com>
2023-03-08 23:04:10 +0500

    js.js modified

--- js.js
+++ js.js
@@ -16,10 +16,14 @@ let numberOfSeries = +prompt("nechta serial kordingiz", ""),
 // seriesDB.series[a] = c
 // seriesDB.series[b] = d
 
+// Bu yerda necha serial korgan bolsa shuni kiritadi va shu boyicha series ga seriallar bilan bergan baxosini actorisga esa bosh qahramonlarni yuklaydi
+
 for (i = 0; i < numberOfSeries; i++) {
     let a = prompt(i + 1 + " - Ko'rgan serialingiz?", ""),
-        c = prompt(i + 1 + " - serialga Nechi baxo berasiz?", "")
+        c = prompt(i + 1 + " - serialga Nechi baxo berasiz?", ""),
+        d = prompt(i + 1 + " - Bosh qaxramon", "")
     seriesDB.series[a] = c
+    seriesDB.actors[a] = d
         // seriesDB.series[b] = d
 }
 
