3b14c5bb2dd787ec2cff50dd11b14e349e2b25d8
Abdulbosit <abdulbosit19980204@gmail.com>
2023-03-09 23:42:43 +0500

    Code optimallashtirildi

--- 15-dars-amaliyot/js.js
+++ 15-dars-amaliyot/js.js
@@ -19,12 +19,29 @@ let numberOfSeries = +prompt("nechta serial kordingiz", ""),
 // Bu yerda necha serial korgan bolsa shuni kiritadi va shu boyicha series ga seriallar bilan bergan baxosini actorisga esa bosh qahramonlarni yuklaydi
 
 for (i = 0; i < numberOfSeries; i++) {
+
     let a = prompt(i + 1 + " - Ko'rgan serialingiz?", ""),
         c = prompt(i + 1 + " - serialga Nechi baxo berasiz?", ""),
         d = prompt(i + 1 + " - Bosh qaxramon", "")
-    seriesDB.series[a] = c
-    seriesDB.actors[a] = d
-        // seriesDB.series[b] = d
-}
 
+    if (a != null && b != null && a != "" && b != "") {
+        seriesDB.series[a] = c;
+        seriesDB.actors[a] = d;
+        console.log("Hamma qiymat to'gri va ma'lumotlar saqlandi");
+    } else {
+        console.log("Natog'ri ma'lumot ma'lumot tufayli toxtatildi");
+        i--;
+        continue;
+    }
+
+
+
+}
+if (seriesDB.count < 5) {
+    console.log("Kam serial ko’ripsiz")
+} else if (seriesDB.count > 5 & seriesDB.count < 10) {
+    console.log("Siz classik tamoshabin ekansiz")
+} else {
+    console.log("Siz serialchi zvezda ekansiz")
+}
 console.log(numberOfSeries, seriesDB);
