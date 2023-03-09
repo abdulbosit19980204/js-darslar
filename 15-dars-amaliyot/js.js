f00cf49dee7c214f1991f4c630ed0333d2e3b6b1
Abdulbosit <abdulbosit19980204@gmail.com>
2023-03-09 23:58:51 +0500

    qayta kirish soraladi

--- 15-dars-amaliyot/js.js
+++ 15-dars-amaliyot/js.js
@@ -8,14 +8,6 @@ let numberOfSeries = +prompt("nechta serial kordingiz", ""),
         privat: false,
     }
 
-// a = prompt("Oxirgi ko'rgan serialingiz?", ""),
-// c = prompt("Nechi baxo berasiz?", ""),
-// b = prompt("Oxirgi ko'rgan serialingiz?", ""),
-// d = prompt("Nechi baxo berasiz?", "")
-
-// seriesDB.series[a] = c
-// seriesDB.series[b] = d
-
 // Bu yerda necha serial korgan bolsa shuni kiritadi va shu boyicha series ga seriallar bilan bergan baxosini actorisga esa bosh qahramonlarni yuklaydi
 
 for (i = 0; i < numberOfSeries; i++) {
@@ -24,22 +16,22 @@ for (i = 0; i < numberOfSeries; i++) {
         c = prompt(i + 1 + " - serialga Nechi baxo berasiz?", ""),
         d = prompt(i + 1 + " - Bosh qaxramon", "")
 
-    if (a != null && b != null && a != "" && b != "") {
+    if (a != null && c != null && d != null && a != "" && c != "" && d != "") {
         seriesDB.series[a] = c;
         seriesDB.actors[a] = d;
         console.log("Hamma qiymat to'gri va ma'lumotlar saqlandi");
     } else {
-        console.log("Natog'ri ma'lumot ma'lumot tufayli toxtatildi");
+        console.log("Natog'ri ma'lumot tufayli toxtatildi");
         i--;
-        continue;
+
     }
 
 
 
 }
-if (seriesDB.count < 5) {
+if (seriesDB.count <= 5) {
     console.log("Kam serial ko’ripsiz")
-} else if (seriesDB.count > 5 & seriesDB.count < 10) {
+} else if (seriesDB.count > 5 & seriesDB.count <= 10) {
     console.log("Siz classik tamoshabin ekansiz")
 } else {
     console.log("Siz serialchi zvezda ekansiz")
