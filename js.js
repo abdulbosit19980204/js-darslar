10fb0608d5716de03a48fd5c3766ba8a806d5388
Abdulbosit <abdulbosit19980204@gmail.com>
2023-03-10 19:21:12 +0500

    17-dars qoshildi

---
+++ js.js
@@ -0,0 +1,23 @@
+"use strike"
+
+const name = "Abdulbosit"
+const text = " How is lorem ipsum created The Lorem Ipsum we know today is derived from parts of the first book Liber Primus and its discussion on hedonism, the words of which had been altered, added and removed to make it nonsensical and improper Latin.  It is not known exactly when the text gained its current traditional form "
+const text1 = "lPariatur minim id aliqua nisi do aute esse labore deserunt velit.Excepteur fugiat amet ut et ipsum est sit. Nostrud nulla consectetur cillum id in eiusmod aliqua. Enim dolor esse officia cillum sunt quis et non pariatur laborum magna aliqua excepteur. Aliquip sunt voluptate ea nostrud id ad. Eiusmod culpa do culpa fugiat cupidatat. Nisi ut proident minim reprehenderit cupidatat deserunt aute anim nulla nulla. Esse esse nostrud enim veniam."
+    // console.log(text1)
+
+// Proporties
+
+// s = text1.length
+// p = text1.toUpperCase();
+// console.log(s, p)
+
+//method - indexOf()
+// for (i = 0; i < text1.length; i++) {
+//     m = text1.indexOf("i")
+//     console.log(m)
+// }
+
+//slice bolaklarga boladi
+thirdText = text.slice(30, (45 * 2))
+console.log(thirdText)
+    // console.log(text1.slice())
