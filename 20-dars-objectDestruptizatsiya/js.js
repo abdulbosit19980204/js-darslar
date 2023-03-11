77cab2c1b4d594e3db7fba15e5e8c93e772531f6
Abdulbosit <abdulbosit19980204@gmail.com>
2023-03-12 00:47:34 +0500

    20 - dars distruptizatsiya

--- 20-dars-objectDestruptizatsiya/js.js
+++ 20-dars-objectDestruptizatsiya/js.js
@@ -7,8 +7,12 @@ const thief = {
         color: "gray",
         style: "curley"
     },
-};
 
+    howOut: function() {
+        console.log("Fastly, via doors")
+    }
+};
+thief.howOut();
 console.log(thief)
 
 //object timiz ni keylarini chiqaramiz Object method
@@ -26,3 +30,25 @@ for (let key in thief) {
         console.log(`Property ${key} has value ${thief[key]}`);
     }
 }
+
+
+// objectdagi elementlar sonini chiqaramiz
+
+// a)
+let count = 0;
+for (let key in thief) {
+    count++;
+}
+console.log(count)
+
+// b)
+console.log(Object.keys(thief).length)
+
+//Destruptizatsiya
+const { color, style } = thief.hair
+console.log(color, style)
+    //odatiy xolda quydagi korinishda olmaiz destruptizatsiya da esa yuqoridagidek bu yerda {bu yerdagi o'zgaruvchilar object ichidagi bilan birxil boladi}
+let hairColor = thief.hair.color,
+    hairStyle = thief.style
+
+console.log(hairColor, style)
