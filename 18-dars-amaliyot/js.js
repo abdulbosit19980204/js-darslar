4a7d7efa9edbb7afadd4f20274a230ff90ccfe0f
Abdulbosit <abdulbosit19980204@gmail.com>
2023-03-11 16:09:18 +0500

    18-dars-amalyot qoshildi

---
+++ 18-dars-amaliyot/js.js
@@ -0,0 +1,72 @@
+let numberOfSeries;
+startApp();
+
+const seriesDB = {
+    count: numberOfSeries,
+    series: {},
+    actors: {},
+    genres: [],
+    privat: false,
+}
+
+function startApp() {
+    numberOfSeries = +prompt("nechta serial kordingiz", "");
+    while (numberOfSeries == "" || numberOfSeries == null || isNaN(numberOfSeries)) {
+        numberOfSeries = +prompt("nechta serial kordingiz", "");
+    }
+}
+
+// Bu yerda necha serial korgan bolsa shuni kiritadi va shu boyicha series ga seriallar bilan bergan baxosini actorisga esa bosh qahramonlarni yuklaydi
+
+for (i = 0; i < numberOfSeries; i++) {
+
+    let a = prompt(i + 1 + " - Ko'rgan serialingiz?", ""),
+        c = prompt(i + 1 + " - serialga Nechi baxo berasiz?", ""),
+        d = prompt(i + 1 + " - Bosh qaxramon", "")
+
+    if (a != null && c != null && d != null && a != "" && c != "" && d != "") {
+        seriesDB.series[a] = c;
+        seriesDB.actors[a] = d;
+        console.log("Hamma qiymat to'gri va ma'lumotlar saqlandi");
+    } else {
+        console.log("Natog'ri ma'lumot tufayli toxtatildi");
+        i--;
+
+    }
+
+
+
+}
+
+
+if (seriesDB.count <= 5) {
+    console.log("Kam serial ko’ripsiz")
+} else if (seriesDB.count > 5 & seriesDB.count <= 10) {
+    console.log("Siz classik tamoshabin ekansiz")
+} else if (seriesDB.count > 10) {
+    console.log("Siz serialchi zvezda ekansiz")
+}
+console.log(numberOfSeries, seriesDB);
+
+let isPrivate = prompt("Qanday personsiz")
+
+function showDb(isPrivate) {
+    if (isPrivate == "private") {
+        console.log("VIP")
+        return true
+    } else if (isPrivate != "") {
+        console.log("Public person")
+        return false
+    } else {
+        console.log("Error")
+    }
+}
+
+showDb(isPrivate)
+
+
+function writeGenres() {
+    for (i = 0; i < numberOfSeries; i++) {
+
+    }
+}
