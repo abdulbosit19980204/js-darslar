c08d4416f18bfee24125360f2f44051e52cf9b86
decoder <abdulbosit19980204@gmail.com>
2023-03-14 08:27:56 +0500

    21-dars massivlar

--- 21-dars-massivlar/js.js
+++ 21-dars-massivlar/js.js
@@ -4,3 +4,19 @@ console.log(a[1])
 for (i = 7; i < a.length; i++) {
     console.log(a[i])
 }
+
+// pop - massiv oxirgi elementini o'chiradi
+a.pop()
+console.log("<pop> ", a, " </pop>")
+
+//push esa oxiriga elemnt qoshadi pushdi ichida kiritilishi kerak bolgan qiymat yoziladi
+a.push("Bu yangi kiritilgan qiymat")
+console.log("<push> ", a, "</push>")
+
+//shift massivni boshida bitta element tashlab ketadi - lekin deyarlik ishlatilmaydi
+a.shift()
+console.log("<shift>", a, "</shift>")
+
+// unshift boshiga element qoshadi
+a.unshift("")
+console.log("<unshift>", a, "</unshift>")
