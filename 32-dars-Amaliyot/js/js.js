3b2ac76927ff682bd180b69a1f9c3b11e1b077d0
decoder <abdulbosit19980204@gmail.com>
2023-03-20 00:09:19 +0500

    qisman done

--- 32-dars-Amaliyot/js/js.js
+++ 32-dars-Amaliyot/js/js.js
@@ -1,41 +1,90 @@
-let promoAdv = document.querySelectorAll('.promo__adv img'),
-    promoGenre = document.querySelector('.promo__genre'),
-    promoBg = document.querySelector('.promo__bg'),
-    promoInteractiveList = document.querySelector('.promo__interactive-list'),
-    btn = document.querySelector('button'),
-    check = document.querySelectorAll('input'),
-    seriesDB = {
-        series: [
-            'Omar',
-            'Songi qaxramon',
-            'Binasa',
-            'Magnificent Century',
-            'The Great Seljuks: Guardians...',
-        ],
+'use strict'
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    let promoAdv = document.querySelectorAll('.promo__adv img'),
+        promoGenre = document.querySelector('.promo__genre'),
+        promoBg = document.querySelector('.promo__bg'),
+        promoInteractiveList = document.querySelector('.promo__interactive-list'),
+        btn = document.querySelector('button'),
+        check = document.querySelectorAll('input'),
+        addForm = document.querySelector('form.add'),
+        inputVal = addForm.querySelector('.adding__input'),
+        checkbox = addForm.querySelector("[type='checkbox']"),
+
+        seriesDB = {
+            series: [
+                'Omar',
+                'Songi qaxramon',
+                'Binasa',
+                'Magnificent Century',
+                'The Great Seljuks: Guardians...',
+            ],
+        }
+
+
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        const newSeries = inputVal.value
+        const accept = checkbox.checked
+        console.log("newSeries = ", newSeries);
+        console.log("accept = ", accept);
+        if (newSeries != "") {
+            seriesDB.series.push(newSeries)
+            seriesDB.series.sort()
+            console.log(seriesDB);
+
+            addListSeries(seriesDB.series, promoInteractiveList)
+        }
+
+
+    })
+
+    check[1].addEventListener('click', () => {
+        if (check[1].checked == false) {
+            btn.style.backgroundColor = 'red'
+        } else {
+            btn.style.backgroundColor = 'green'
+        }
+    })
+
+    function addListSeries(series, parent) {
+        parent.innerHTML = ""
+
+        series.forEach((item, idx) => {
+            parent.innerHTML += `
+            <li class="promo__interactive-item"> ${idx + 1} ${item}
+            <div class="delete"></div>
+            </li>
+            `
+        })
     }
-promoAdv.forEach((item) => {
-    item.remove()
+
+    function deleteAd(arr) {
+
+        promoAdv.forEach((item) => {
+            item.remove()
+        })
+
+        promoGenre.textContent = "Comedy"
+
+        promoBg.style.backgroundImage = 'url("img/1.jpg")'
+    }
+
+    function btncheck() {
+        btn.style.borderRadius = '8px'
+
+        console.log(check[1].checked);
+        if (check[1].checked == false) {
+            btn.style.backgroundColor = 'red'
+        } else {
+            btn.style.backgroundColor = 'green'
+        }
+    }
+    const sortArr = (arr) => {
+        arr.sort()
+    }
+
+    deleteAd(promoAdv)
+    btncheck()
 })
-
-promoGenre.textContent = "Comedy"
-
-promoBg.style.backgroundImage = 'url("img/1.jpg")'
-promoInteractiveList.innerHTML = ""
-
-seriesDB.series.forEach((item, idx) => {
-    promoInteractiveList.innerHTML += `
-    <li class="promo__interactive-item"> ${idx + 1} ${item}
-    <div class="delete"></div>
-    </li>
-    `
-})
-
-btn.style.borderRadius = '8px'
-
-
-console.log(check[1].checked);
-if (check[1].checked == false) {
-    btn.style.backgroundColor = 'red'
-} else {
-    btn.style.backgroundColor = 'green'
-}
