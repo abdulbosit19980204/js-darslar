58a9ad6a9c6f989f044bec44f355451dbfe59b43
decoder <abdulbosit19980204@gmail.com>
2023-03-19 17:49:44 +0500

    deyarlik done

--- 30-dars-xodisalar/js/js.js
+++ 30-dars-xodisalar/js/js.js
@@ -1,7 +1,13 @@
 'use strict'
-
-// function hello() {
-//     alert(prompt("Ismingizni kiriting") + " welcome")
+let btns = document.querySelector('#btn'),
+    btn1c = document.querySelector('#btn1'),
+    bodycl = document.querySelector('body'),
+    dlt = document.querySelector('#delete'),
+    lnk = document.querySelector('#alink'),
+    appn = document.querySelector('#appendN'),
+    evbtn = document.querySelector('#eventBtn')
+    // function hello() {
+    //     alert(prompt("Ismingizni kiriting") + " welcome")
 
 // }
 
@@ -11,11 +17,60 @@
 // }
 // console.log(btn);
 
-const btnclic = document.querySelector('#btn')
-    // btnclic.onclick = function() {
-    //     alert('hello')
-    // }
 
-btnclic.addEventListener('click', () => {
-    alert("Hello")
+// btnclic.onclick = function() {
+//     alert('hello')
+// }
+
+btns.addEventListener('click', () => {
+    alert("va nihoyat")
 })
+btn1c.addEventListener('mouseenter', (event) => {
+    bodycl.style.backgroundColor = " #ff05ff"
+})
+dlt.addEventListener('click', () => {
+    lnk.remove()
+    alert("o'chilirdi")
+})
+
+// appn.addEventListener('click', () => {
+//     bodycl.append(lnk)
+//     dlt.before(btns, btn1c)
+//     alert("elementlar qayta qoshildi")
+
+// })
+
+
+const deleteElement = (evnt) => {
+    evnt.target.remove()
+    btns.remove()
+}
+btn1c.addEventListener('click', deleteElement)
+
+let i = 0
+const addElement = (evnt) => {
+    i++
+    alert("yana " + (3 - i) + " ta urunishingiz qoldi")
+    if (i === 3) {
+        evbtn.removeEventListener('click', addElement)
+        alert("urinishlaringiz  tugagan")
+    }
+    console.log(i)
+}
+evbtn.addEventListener('click', addElement)
+
+let eventCounter = 0
+let addBtn = (evnt) => {
+    eventCounter++
+
+    bodycl.append(lnk)
+    dlt.before(btns, btn1c)
+    alert((eventCounter) + " - marta elementlar qayta qoshildi")
+
+    if (eventCounter === 3) {
+        appn.removeEventListener('click', addBtn)
+        alert('Bugungi urunishlariz tugadi')
+    }
+}
+
+appn.addEventListener('click', addBtn)
