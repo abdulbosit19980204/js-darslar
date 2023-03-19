6c6bc65c9b8fe4901cef9251d07221612309055c
decoder <abdulbosit19980204@gmail.com>
2023-03-19 15:04:09 +0500

    Amaliyot is done

--- 29-Amaliyot-DOM/js/script.js
+++ 29-Amaliyot-DOM/js/script.js
@@ -1,22 +1,22 @@
 'use strict'
 
 const adv = document.querySelectorAll('.promo__adv img'),
-  wrapper = document.querySelector('.promo__bg'),
-  genre = wrapper.querySelector('.promo__genre'),
-  seriesList = document.querySelector('.promo__interactive-list')
+    wrapper = document.querySelector('.promo__bg'),
+    genre = wrapper.querySelector('.promo__genre'),
+    seriesList = document.querySelector('.promo__interactive-list')
 
 const seriesDB = {
-  series: [
-    'Omar',
-    'The Final Legacy',
-    'Ertugrul',
-    'Magnificent Century',
-    'The Great Seljuks: Guardians...',
-  ],
+    series: [
+        'Omar',
+        'The Final Legacy',
+        'Ertugrul',
+        'Magnificent Century',
+        'The Great Seljuks: Guardians...',
+    ],
 }
 
 adv.forEach((item) => {
-  item.remove()
+    item.remove()
 })
 
 genre.textContent = 'comedy'
@@ -26,7 +26,7 @@ wrapper.style.backgroundImage = 'url("img/1.jpg")'
 seriesList.innerHTML = ''
 
 seriesDB.series.forEach((item, idx) => {
-  seriesList.innerHTML += `
+    seriesList.innerHTML += `
     <li class="promo__interactive-item">${idx + 1} ${item}
       <div class="delete"></div>
     </li>
