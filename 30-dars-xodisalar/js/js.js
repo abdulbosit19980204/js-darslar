3c9afd11dec2a6bdf19efeb1165ec2b6b7e7d551
decoder <abdulbosit19980204@gmail.com>
2023-03-19 16:07:59 +0500

    addevent

---
+++ 30-dars-xodisalar/js/js.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// function hello() {
+//     alert(prompt("Ismingizni kiriting") + " welcome")
+
+// }
+
+// const btn = document.querySelector('#btn')
+// btn.onclick = function() {
+//     alert('hello')
+// }
+// console.log(btn);
+
+const btnclic = document.querySelector('#btn')
+    // btnclic.onclick = function() {
+    //     alert('hello')
+    // }
+
+btnclic.addEventListener('click', () => {
+    alert("Hello")
+})
