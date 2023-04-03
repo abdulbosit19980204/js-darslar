17ea8a0e3cde571502d955264bea0ed23edf5646
decoder <abdulbosit19980204@gmail.com>
2023-04-03 16:34:06 +0500

    ayrim fayllar almashtirlidi

---
+++ 57-dars-DynamicStyling/js/time.js
@@ -0,0 +1,58 @@
+const deadline = '2023-03-29 07:56:00'
+
+
+
+function getTimeRemaining(endtime) {
+    const timer = Date.parse(endtime) - Date.parse(new Date())
+    days = Math.floor(timer / (1000 * 60 * 60 * 24))
+    hours = Math.floor((timer / (1000 * 60 * 60)) % 24)
+    minutes = Math.floor((timer / 1000 / 60) % 60)
+    seconds = Math.floor((timer / 1000) % 60)
+    console.log(seconds);
+    return {
+        timer,
+        days,
+        hours,
+        minutes,
+        seconds
+    }
+
+
+}
+
+function getZero(num) {
+    if (num >= 0 && num < 10) {
+        return `0${num}`
+    } else {
+        return num
+    }
+}
+
+
+function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+        days = timer.querySelector('#days'),
+        hours = timer.querySelector('#hours'),
+        minutes = timer.querySelector('#minutes'),
+        seconds = timer.querySelector('#seconds'),
+        timeInterval = setInterval(updateClock, 1000)
+    updateClock()
+
+    function updateClock() {
+        const t = getTimeRemaining(endtime)
+        days.innerHTML = getZero(t.days)
+        hours.innerHTML = getZero(t.hours)
+        minutes.innerHTML = getZero(t.minutes)
+        seconds.innerHTML = getZero(t.seconds)
+
+        if (t.timer <= 0) {
+            clearInterval(timeInterval)
+            const endingMessage = document.querySelector('.timer')
+            endingMessage.textContent = "Afsus Chegirma tugadi!"
+            endingMessage.style.fontSize = '40px'
+            endingMessage.style.color = 'red'
+        }
+    }
+}
+
+setClock('.timer', deadline)
