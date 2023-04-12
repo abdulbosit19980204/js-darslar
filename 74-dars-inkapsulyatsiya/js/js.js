cf3ca064382e695f89be12a8fb6206b36c9febf6
decoder <abdulbosit19980204@gmail.com>
2023-04-12 06:54:50 +0500

    Inkapsulyatsiya

--- 74-dars-inkapsulyatsiya/js/js.js
+++ 74-dars-inkapsulyatsiya/js/js.js
@@ -1,7 +1,14 @@
 'use strict'
 
-function car() {
-    name: "BMW",
-    color: "Black",
+function Car(name, color) {
+   this.name = name
+   this.color =color
+
+   this.info = function(){
+     console.log(`Name of ${this.name}, color is ${this.color}`);
+   }
 
 }
+
+const bwm = new Car("BMW", "Black")
+bwm.info() // Name of BMW, color is Black
